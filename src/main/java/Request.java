34b9cce0d56029672f737766f51c7a44716de1b9
Алексей Печин <organner@mail.ru>
2022-09-25 15:52:50 +0300

    Adding the getQueryParams() method; Adding the getQueryParams(String name) method; Adding the setQueryParams(List<NameValuePair> queryParams) method

--- src/main/java/Request.java
+++ src/main/java/Request.java
@@ -1,10 +1,14 @@
+import org.apache.http.NameValuePair;
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Request {
 
     private final RequestLine requestLine;
     private final List<String> headers;
     private String body;
+    private List<NameValuePair> queryParams;
 
     public Request(RequestLine requestLine, List<String> headers) {
         this.requestLine = requestLine;
@@ -25,6 +29,21 @@ public class Request {
 
     public void setBody(String body) {
         this.body = body;
+    }
+
+    public List<NameValuePair> getQueryParams() {
+        return queryParams;
+    }
+
+    public List<String> getQueryParam(String name) {
+        return queryParams.stream()
+                .filter(o -> o.getName().startsWith(name))
+                .map(NameValuePair::getValue)
+                .collect(Collectors.toList());
+    }
+
+    public void setQueryParams(List<NameValuePair> queryParams) {
+        this.queryParams = queryParams;
     }
 
     @Override
